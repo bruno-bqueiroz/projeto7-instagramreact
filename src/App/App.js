42963c91d2610384fbad1e6469e87e85b1e819c5
BRUNO BARBOSA <brunobdq32@gmail.com>
2022-07-14 08:41:16 -0400

    todos os arquivos

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import Topo from "./Topo";
+import Menu from "./Menu";
+
+export default function App() {
+    return (
+        <div>
+			<Topo />
+			<Menu />
+        </div>
+    );
+}
+
+
+
+
---
+++ src/App/Topo.js
@@ -0,0 +1,7 @@
+export default function Topo() {
+    return (
+        <div>
+           
+        </div>
+    );
+}
